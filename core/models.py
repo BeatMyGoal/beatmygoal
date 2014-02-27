6e8b7dfe919d4e952e914b83646187a540c7a301
Arjun Rao <a.rao456@gmail.com>
2014-02-26 19:28:35 -0800

    changed goal progress field

--- core/models.py
+++ core/models.py
@@ -9,7 +9,7 @@ class Goal(models.Model):
     description = models.TextField()
     prize = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
-    progress_value = models.IntegerField()
+    progress_value = models.FloatField()
     
 class Participant(models.Model):
     user = models.OneToOneField(User)
