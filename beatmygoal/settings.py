0a3aca44aefbcfe89124c4939fe63b52a89bf67b
Tim Hyon <Tim@Tims-MacBook-Air-2.local>
2014-03-01 15:31:35 -0800

    fixing heroku deployment

--- beatmygoal/settings.py
+++ beatmygoal/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'south',
     'core',
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
