6f521f87ea178522409ed559feb070c06b37e0d9
Tim Hyon <Tim@Tims-MacBook-Air-2.local>
2014-03-01 17:02:17 -0800

    fixing static files

--- beatmygoal/settings.py
+++ beatmygoal/settings.py
@@ -110,6 +110,10 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '../static'),
 )
+
+print STATICFILES_DIRS
+
