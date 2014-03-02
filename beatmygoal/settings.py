3baa1d17c200eac84c158e6e49f340b8b67aa25c
Tim Hyon <Tim@Tims-MacBook-Air-2.local>
2014-03-01 17:14:34 -0800

    fixing static files

--- beatmygoal/settings.py
+++ beatmygoal/settings.py
@@ -107,7 +107,7 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = '/static/'
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 
 
