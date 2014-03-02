86b4bf72ede86a9355f8c83ec7136e0ac4fb1b11
Hyun Ho <kknd113@hotmail.com>
2014-03-01 17:13:03 -0800

    modified views.py

--- core/views.py
+++ core/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from models import Goal, Participant
 
+from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_exempt
+import json
 # Create your views here.
 
 def test(request):
-    return render(request, 'index.html', {"foo": "bar"})
+    return render(request, 'base.html', {"foo": "bar"})
+
+
+def goal_delete_goal(request):
+	try:
+		req = json.loads(request.body)
+		goal_id = req["goal_id"]
+	except:
+		return request.send_error(500)
+	response = Goal().delete_goal(goal_id)
+	return HttpResponse(json.dumps({"errCode": response}), content_type = "application/json")
+
+def goal_remove_user(request):
+	try:
+		req = json.loads(request.body)
+		goal_id = req["goal_id"]
+		user_id = req["user_id"]
+	except:
+		return requeset.send_error(500)
+	response = Goal().remove_user(goal_id, user_id)
+	return HttpResponse(json.dumps({"errCode": response}), content_type = "application/json")
+
+
+
