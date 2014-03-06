67dc4d4cdfe395f82ef7533ad2af9c46aab35064
Hyun Ho <kknd113@hotmail.com>
2014-03-06 14:21:49 -0800

    fixed bug in views

--- core/views.py
+++ core/views.py
@@ -29,8 +29,8 @@ def goal_create_goal(request):
 	description = data['description']
 	creator = data['creator']
 	prize = data['prize']
-	private_setting = req['private_setting']
-	goal_type = req['goal_type']
+	private_setting = data['private_setting']
+	goal_type = data['goal_type']
 	response = Goal.create(title, description, creator, prize, private_setting, goal_type)
 	return HttpResponse(json.dumps({"errCode": response}), content_type = "application/json")
 
