e6ae9e6634c050ca6770adf17c8b279a00cddaba
Jay Patel <patel.jay@berkeley.edu>
2014-03-10 19:44:18 -0700

    got frontend registration form working

--- static/js/users/createUser.js
+++ static/js/users/createUser.js
@@ -1,34 +1,44 @@
 $(document).ready(function() {
-    
-    $("#register-submit").click(function(e) {
-	console.log("clicked!");
-        //e.preventDefault();
-        //e.stopPropagation();
 
-	var data = {
-	    username : $("#register-username").val(),
-	    password : $("#register-password").val(),
-	    email: $("#register-email").val(),
-	};
+	$("#register-submit").click(function(e) {
+		
+		var data = {
+			username: $("#register-username").val(),
+			password: $("#register-password").val(),
+			email: $("#register-email").val(),
+		};
 
-	$.ajax({
-	    type: "POST",
-	    url: "",
-	    data: JSON.stringify(data),
-	    contentType: "application/json",
-	    dataType: "json",
-	}).done(function(data) {
-		console.log(data);
-	    if (data.hasOwnProperty("success")) {
-			console.log(data.redirect);
-			window.location.href = data.redirect;   
-	    } else {
-		// show the errors
-	    }
-	    
-	}).fail(function(data) {
-	    alert("failure");
+		$.ajax({
+			type: "POST",
+			url: "/users/create",
+			data: JSON.stringify(data),
+			contentType: "application/json",
+			dataType: "json",
+		}).done(function(data) {
+			console.log(data);
+			if (data.hasOwnProperty("success")) {
+				window.location.href = data.redirect;
+			} else {
+				if ('errors' in data) {
+					var errors = data.errors;
+					if ('username' in errors) {
+						$('#username-error').text(data.errors.username).show();
+
+					}
+					if ('email' in errors) {
+						$('#email-error').text(data.errors.email).show();
+
+					}
+					if ('password' in errors) {
+						$('#password-error').text(data.errors.password).show();
+
+					}
+				}
+			}
+
+		}).fail(function(data) {
+			alert("failure");
+		});
 	});
-    });
-    
+
 });
