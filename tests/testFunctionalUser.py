5b8d4631cc1d26bdf99f2b0d17d57ed6a2bcad42
HyosangKim <khs8727@berkeley.edu>
2014-03-13 23:18:02 -0700

    viewUser functional test

--- tests/testFunctionalUser.py
+++ tests/testFunctionalUser.py
@@ -32,39 +32,48 @@ class RegistrationTests(TestCase):
 
 class ViewUserTests(TestCase):
     def setUp(self):
-        self.client = Client()
+        self.cleint = Client()
+        self.testUser = BeatMyGoalUser(username="kyle", password="123", email="kyle@test.com")
+        self.testUser.save()
 
-    # Convenience method to create a POST JSON request
-    def postJSON(self, url, data):
-        return self.client.post(url, content_type='application/json', data=data)
-
-    def testViewPageLoads(self):
+    def testUserView(self):
         """
         Tests to make sure the view page loads
         """
-        response = self.client.get("/users/1/")
+        userid = self.testUser.id
+        username = self.testUser.username
+        email = self.testUser.email
+        response = self.client.get("/users/" + str(userid)+ "/", {})
         self.assertEqual(response.status_code, 200)
-
-    def testLoadUserView(self):
-        data = """
-        { "username" : "kyle", "password" : "123", "email" : "kyleinwa@gmail.com" }
-        """
-        response = self.postJSON("/users/create", data)
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue('success' in response.content)
-
 
 
 class DeleteUserTests(TestCase):
     def setUp(self):
         self.client = Client()
-        self.testUser = BeatMyGoalUser(username="test", password="test", email="test@test.com")
+        self.testUser = BeatMyGoalUser(username="kyle", password="123", email="kyle@test.com")
+        self.testUser.save()
 
     def postJSON(self, url, data):
         return self.client.post(url, content_type='application/json', data=data)
 
+    def testDeleteWrong(self):
+        """
+        Tests that a user cannot be delete if not logged in
+        """
+        response = self.postJSON("/users/" + str(self.testUser.id) +"/delete", {})
+        self.assertEqual(response.status_code, 500)
+
     def testDeleteUser(self):
-        pass
+        """
+        Tests that a user can be delete User if logged in
+        """
+        data = """
+        { "username" : "kyle", "password" : "123" }
+        """
+        response = self.postJSON("/users/login", data)
+        response2 = self.postJSON("/users/" + str(self.testUser.id) + "/delete", {})
+        self.assertEqual(response2.status_code, 200)
+
 
 
 class EditUserTests(TestCase):
