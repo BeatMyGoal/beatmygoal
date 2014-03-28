9bbfba7a495305f107b36af2fe5b7d6d9b98b0d4
Tim Hyon <Tim@tims-air-2.home>
2014-03-28 08:30:31 -0400

    testing heroku lazyload error 2

--- static/js/dashboard/dashboard.js
+++ static/js/dashboard/dashboard.js
@@ -1,77 +1,76 @@
 $(document).ready(function() {
-var page = 0;
-var scroll_activated = false;
+	var page = 0;
+	var scroll_activated = false;
 
-var data = {
-	page: page,
-$.ajax({
-	type: "POST",
-	url: "/dashboard",
-	data: JSON.stringify(data),
-	contentType: "application/json",
-	dataType: "json",
-}).done(function(data) {
-	goals = jQuery.parseJSON(data['goals']);
-	goals.forEach(function(entry) {
-		console.log(entry);
-		fields = entry['fields'];
-		user = $(".users>#id"+fields['creator']);
-		console.log(fields);
-		$(".dashcard-container").append('<div class="dashcard" id="'+entry['pk']+'"></div>');
-		$(".dashcard#"+entry['pk']).append('<div class=dashcard-title><a href="/goals/'+entry['pk']+'">'+fields['title']+'</a></div>');
-		$(".dashcard#"+entry['pk']).append('<div class=dashcard-creator>Creator: <a href="/users/'+fields['creator']+'">'+user.text()+'</a></div>');
-		$(".dashcard#"+entry['pk']).append('<div class=dashcard-prize>Prize: '+fields['prize']+'</div>');
-		$(".dashcard#"+entry['pk']).append('<div class=dashcard-description>'+fields['description']+'</div>');
-		// $("tbody").append('<tr><td><a href="/goals/'+entry['pk']+'">'+fields['title']+'</a></td>'+
-		// 	'<td>'+fields['description']+'</td>'+
-		// 	'<td><a href="/users/'+fields['creator']+'">'+user.text()+'</a></td>'+
-		// 	'<td>'+fields['prize']+'</td></tr>')
-	});
-
-}).fail(function(data) {
-	console.log(data);
-	alert("failure");
-});
-page = 1;
-};
-
-
-$(window).scroll(function() {
-   if($(window).scrollTop() + $(window).height() == $(document).height() && !scroll_activated) {
-        scroll_activated = true;
-		var data = {
-			page: page,
-		};
-		$.ajax({
-			type: "POST",
-			url: "/dashboard",
-			data: JSON.stringify(data),
-			contentType: "application/json",
-			dataType: "json",
-		}).done(function(data) {
-			goals = jQuery.parseJSON(data['goals']);
-			goals.forEach(function(entry) {
-				console.log(entry);
-				fields = entry['fields'];
-				user = $(".users>#id"+fields['creator']);
-				console.log(user);
-				$(".dashcard-container").append('<div class=dashcard id="'+entry['pk']+'"></div>');
-				$(".dashcard#"+entry['pk']).append('<div class=dashcard-title><a href="/goals/'+entry['pk']+'">'+fields['title']+'</a></div>');
-				$(".dashcard#"+entry['pk']).append('<div class=dashcard-creator>Creator: <a href="/users/'+fields['creator']+'">'+user.text()+'</a></div>');
-				$(".dashcard#"+entry['pk']).append('<div class=dashcard-prize>Prize: '+fields['prize']+'</div>');
-				$(".dashcard#"+entry['pk']).append('<div class=dashcard-description>'+fields['description']+'</div>');
-				// $("tbody").append('<tr><td><a href="/goals/'+entry['pk']+'">'+fields['title']+'</a></td>'+
-				// 	'<td>'+fields['description']+'</td>'+
-				// 	'<td><a href="/users/'+fields['creator']+'">'+user.text()+'</a></td>'+
-				// 	'<td>'+fields['prize']+'</td></tr>');
-			});
-
-		}).fail(function(data) {
-			console.log(data);
-			alert("failure");
+	var data = {
+		page: page,
+	};
+	$.ajax({
+		type: "POST",
+		url: "/dashboard",
+		data: JSON.stringify(data),
+		contentType: "application/json",
+		dataType: "json",
+	}).done(function(data) {
+		goals = jQuery.parseJSON(data['goals']);
+		goals.forEach(function(entry) {
+			console.log(entry);
+			fields = entry['fields'];
+			user = $(".users>#id"+fields['creator']);
+			console.log(fields);
+			$(".dashcard-container").append('<div class="dashcard" id="'+entry['pk']+'"></div>');
+			$(".dashcard#"+entry['pk']).append('<div class=dashcard-title><a href="/goals/'+entry['pk']+'">'+fields['title']+'</a></div>');
+			$(".dashcard#"+entry['pk']).append('<div class=dashcard-creator>Creator: <a href="/users/'+fields['creator']+'">'+user.text()+'</a></div>');
+			$(".dashcard#"+entry['pk']).append('<div class=dashcard-prize>Prize: '+fields['prize']+'</div>');
+			$(".dashcard#"+entry['pk']).append('<div class=dashcard-description>'+fields['description']+'</div>');
+			// $("tbody").append('<tr><td><a href="/goals/'+entry['pk']+'">'+fields['title']+'</a></td>'+
+			// 	'<td>'+fields['description']+'</td>'+
+			// 	'<td><a href="/users/'+fields['creator']+'">'+user.text()+'</a></td>'+
+			// 	'<td>'+fields['prize']+'</td></tr>')
 		});
-		scroll_activated = false;
-		page = page + 1;	
-   }
-});
+
+	}).fail(function(data) {
+		console.log(data);
+		alert("failure");
+	});
+	page = 1;
+
+	$(window).scroll(function() {
+	   if($(window).scrollTop() + $(window).height() == $(document).height() && !scroll_activated) {
+	        scroll_activated = true;
+			var data = {
+				page: page,
+			};
+			$.ajax({
+				type: "POST",
+				url: "/dashboard",
+				data: JSON.stringify(data),
+				contentType: "application/json",
+				dataType: "json",
+			}).done(function(data) {
+				goals = jQuery.parseJSON(data['goals']);
+				goals.forEach(function(entry) {
+					console.log(entry);
+					fields = entry['fields'];
+					user = $(".users>#id"+fields['creator']);
+					console.log(user);
+					$(".dashcard-container").append('<div class=dashcard id="'+entry['pk']+'"></div>');
+					$(".dashcard#"+entry['pk']).append('<div class=dashcard-title><a href="/goals/'+entry['pk']+'">'+fields['title']+'</a></div>');
+					$(".dashcard#"+entry['pk']).append('<div class=dashcard-creator>Creator: <a href="/users/'+fields['creator']+'">'+user.text()+'</a></div>');
+					$(".dashcard#"+entry['pk']).append('<div class=dashcard-prize>Prize: '+fields['prize']+'</div>');
+					$(".dashcard#"+entry['pk']).append('<div class=dashcard-description>'+fields['description']+'</div>');
+					// $("tbody").append('<tr><td><a href="/goals/'+entry['pk']+'">'+fields['title']+'</a></td>'+
+					// 	'<td>'+fields['description']+'</td>'+
+					// 	'<td><a href="/users/'+fields['creator']+'">'+user.text()+'</a></td>'+
+					// 	'<td>'+fields['prize']+'</td></tr>');
+				});
+
+			}).fail(function(data) {
+				console.log(data);
+				alert("failure");
+			});
+			scroll_activated = false;
+			page = page + 1;	
+	   }
+	});
 });
