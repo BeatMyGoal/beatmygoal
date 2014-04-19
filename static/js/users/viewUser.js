d8f0c09096b58dfd2ce30123f13777735ec6578a
Tim Hyon <tim.c.hyon@gmail.com>
2014-04-19 16:59:44 -0700

    [ViewUser] frontend tweaks to edit buttons and image upload overlay

--- static/js/users/viewUser.js
+++ static/js/users/viewUser.js
@@ -1,72 +1,71 @@
-$(document).ready(function() {
-});
 function showfileButton(){
-    $('#filebuttonContainer').show();
-    $('#submitbuttonContainer').show();
     $('#dummyContainer').show();
 }
 function hidefileButton(){
-    $('#filebuttonContainer').hide();
-    $('#submitbuttonContainer').hide();
     $('#dummyContainer').hide();
 }
 function submitimage(){
     $('#imagesubmitbutton').click();
 }
+$(document).ready(function() {
+    $("#imdummy").click(function(event) {
+        $("#imagebutton").click();
+    });
 
-$("#reconfirom_delete_user #Yes_button").click(function(event) {
-	var uid = window.location.pathname.split("/")[2];
-	$.ajax({
-		type: "POST",
-		url: "/users/" + uid + "/delete",
-		contentType: "application/json",
-		dataType: "json"
-	}).done(function(data) {
-		window.location.href = data.redirect;
-	}).fail(function(data) {
-		alert("Delete User Fail");
-	});
+    $("#reconfirom_delete_user #Yes_button").click(function(event) {
+        var uid = window.location.pathname.split("/")[2];
+        $.ajax({
+            type: "POST",
+            url: "/users/" + uid + "/delete",
+            contentType: "application/json",
+            dataType: "json"
+        }).done(function(data) {
+            window.location.href = data.redirect;
+        }).fail(function(data) {
+            alert("Delete User Fail");
+        });
 
-});
+    });
 
-$("#reconfirom_delete_user #No_button").click(function(e) {
-	$('#reconfirom_delete_user').foundation('reveal', 'close');
-});
-
-
-$("#reveal_edit #Confirm_button").click(function(e) {
-    //window.location = window.location.href+"edit";
-    //return false;
-    var password = $('#reveal_edit #password').val();
-    var data = { password: password }
-
-    $.ajax({
-        type: "POST",
-        url: "/confirm",
-        data: JSON.stringify(data),
-        contentType: "application/json",
-        dataType: "json",
-    }).done(function(data) {
+    $("#reconfirom_delete_user #No_button").click(function(e) {
+        $('#reconfirom_delete_user').foundation('reveal', 'close');
+    });
 
 
-        if (data.errors.length === 0) {
-            window.location = window.location.href+"edit";
-        } else {
-            if (data.errors.length >= 0) {
-                var errors = data.errors;
-                if (errors.indexOf(ERRCODES.CODE_BAD_PASSWORD) >= 0) {
-                        $('#password-error').text('Validation failed');
-                        $("label[for='password']").addClass("error");
-                    }
+    $("#reveal_edit #Confirm_button").click(function(e) {
+        //window.location = window.location.href+"edit";
+        //return false;
+        var password = $('#reveal_edit #password').val();
+        var data = { password: password }
+
+        $.ajax({
+            type: "POST",
+            url: "/confirm",
+            data: JSON.stringify(data),
+            contentType: "application/json",
+            dataType: "json",
+        }).done(function(data) {
+
+
+            if (data.errors.length === 0) {
+                window.location = window.location.href+"edit";
+            } else {
+                if (data.errors.length >= 0) {
+                    var errors = data.errors;
+                    if (errors.indexOf(ERRCODES.CODE_BAD_PASSWORD) >= 0) {
+                            $('#password-error').text('Validation failed');
+                            $("label[for='password']").addClass("error");
+                        }
+                }
             }
-        }
-    }).fail(function(data) {
-        alert(data);
+        }).fail(function(data) {
+            alert(data);
+        });
+    });
+
+    $("#reveal_edit #Back_button").click(function(e) {
+        $('#reveal_edit').foundation('reveal', 'close');
     });
 });
 
-$("#reveal_edit #Back_button").click(function(e) {
 
-    $('#reveal_edit').foundation('reveal', 'close');
-    
-});
