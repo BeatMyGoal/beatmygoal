ecc84468d15c5d938732e0dcd3b815f654a571be
Jay Patel <patel.jay@berkeley.edu>
2014-05-01 22:58:04 -0700

    try to fix static files

--- beatmygoal/settings.py
+++ beatmygoal/settings.py
@@ -47,6 +47,10 @@ if 'ON_HEROKU' in os.environ:
     STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
     ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
     MEDIA_URL = S3_URL + "/"
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, "static"),
+    )
 else:
     STATIC_URL = '/static/'
     STATICFILES_DIRS = (
